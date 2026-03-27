18aa6ac42dc0bbb6b7e50c3f0ca758682f322069
Sahariyar Prince <sahariyarprince609@gmail.com>
2026-03-27 19:57:39 +0600

    Push my code

---
+++ Modul-18.5 Array/concat.js
@@ -0,0 +1,4 @@
+const array1 = ['Jamil, Kamil, hamil'];
+const array2 = ['Amil, Namil, Zamil']
+const array3 = array1.concat(array2)
+console.log(array3)
--- Modul-18.5 Array/includes.js
+++ Modul-18.5 Array/includes.js
@@ -1,7 +1,7 @@
 const friend = ['jamal', 'kamal', 'namal', 'khalam', 'pailam'];
 console.log(friend.includes('jamal'))
 console.log(friend.includes('halal'))
-if(friend.includes('kamol')){
+if(friend.includes('kamal')){
     console.log('We are friends')
 }
 else{
